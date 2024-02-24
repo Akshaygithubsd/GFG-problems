d22bfc6112b0f71dc96bb1703d8a4b1717d9111b
Akshaygithubsd <103135577+Akshaygithubsd@users.noreply.github.com>
2024-02-24 18:26:39 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Smallest Positive missing number/smallest-positive-missing-number.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to find the smallest positive number missing from the array.
+    int missingNumber(int arr[], int n) 
+    { 
+        // Your code here
+        unordered_set<int>s(arr,arr+n);
+        int res=-1;
+        for(int i=0;i<n;i++){
+            if(s.find(i+1)==s.end()){
+                res=(i+1);
+                break;
+            }
+        }if(res==-1){
+            res=n+1;
+        }
+        return res;
+    } 
+};
+
+//{ Driver Code Starts.
+
+int missingNumber(int arr[], int n);
+
+int main() { 
+    
+    //taking testcases
+    int t;
+    cin>>t;
+    while(t--){
+        
+        //input number n
+        int n;
+        cin>>n;
+        int arr[n];
+        
+        //adding elements to the array
+        for(int i=0; i<n; i++)cin>>arr[i];
+        
+        Solution ob;
+        //calling missingNumber()
+        cout<<ob.missingNumber(arr, n)<<endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
