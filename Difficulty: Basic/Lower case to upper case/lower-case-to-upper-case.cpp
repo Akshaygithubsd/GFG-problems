43bfba9d8cb5700ab790537368dcf73c07bd9086
Akshay_bharadwaj <103135577+Akshaygithubsd@users.noreply.github.com>
2024-09-25 19:17:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Lower case to upper case/lower-case-to-upper-case.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+string to_upper(string in);
+ 
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		string str;
+		cin>>str;
+		cout << to_upper(str)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
+
+
+
+string to_upper(string str){
+    //code
+    string res=str;
+    transform(res.begin(),res.end(),res.begin(),::toupper);
+    return res;
+    fo
+}
