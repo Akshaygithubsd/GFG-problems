63a1227117df4642e5afd160366f3b4ef34fc4d5
Akshay_bharadwaj <103135577+Akshaygithubsd@users.noreply.github.com>
2024-10-19 00:05:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Remove vowels from string/remove-vowels-from-string.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+		
+	string removeVowels(string s) 
+	{
+	    // Your code goes here
+	    string res="";
+	    for(int i=0;i<s.length();i++){
+	        if(s[i]=='a' ||s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u'){
+	            continue;
+	        }else{
+	            res+=s[i];
+	        }
+	    }return res;
+	}
+};
+
+//{ Driver Code Starts.
+
+
+int main() 
+{
+   	
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	string tc;
+   	getline(cin, tc);
+   	t = stoi(tc);
+   	while(t--)
+   	{
+   		string s, ch; 
+
+	    getline(cin, s);  
+   		
+       
+   		Solution ob;
+   		cout << ob.removeVowels(s) << "\n";
+   		
+   	}
+
+    return 0;
+}
+// } Driver Code Ends
